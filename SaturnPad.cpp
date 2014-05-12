97b99e799f52130cb129e70e002098013335a2fb
Andrei Ostanin <aostanin@gmail.com>
2014-05-12 23:03:26 +0900

    initial commit

---
+++ SaturnPad.cpp
@@ -0,0 +1,110 @@
+#include "SaturnPad.h"
+
+SaturnPad::SaturnPad(uint8_t s0_pin, uint8_t s1_pin, uint8_t d0_pin, uint8_t d1_pin, uint8_t d2_pin, uint8_t d3_pin)
+{
+  _s_pins[0] = s0_pin;
+  _s_pins[1] = s1_pin;
+  _d_pins[0] = d0_pin;
+  _d_pins[1] = d1_pin;
+  _d_pins[2] = d2_pin;
+  _d_pins[3] = d3_pin;
+}
+
+void SaturnPad::begin()
+{
+  for (uint8_t i = 0; i < 2; i++)
+    pinMode(_s_pins[i], OUTPUT);
+  for (uint8_t i = 0; i < 4; i++)
+    pinMode(_d_pins[i], INPUT_PULLUP);
+
+  _pad_type = TYPE_NONE;
+}
+
+void SaturnPad::loop()
+{
+  setType();
+
+  switch (_pad_type) {
+    case TYPE_NONE:
+    case TYPE_DIGITAL:
+      readDigital();
+      break;
+
+    case TYPE_ANALOG:
+      readAnalog();
+      break;
+  }
+}
+
+SaturnPad::PadType SaturnPad::padType()
+{
+  return _pad_type;
+}
+
+SaturnPad::DigitalPadState SaturnPad::digitalPadState()
+{
+  return _digital_pad_state;
+}
+
+SaturnPad::AnalogPadState SaturnPad::analogPadState()
+{
+  return _analog_pad_state;
+}
+
+void SaturnPad::setType()
+{
+  switch (_digital_pad_state.id) {
+    case 0x4: // Standard pad
+      _pad_type = TYPE_DIGITAL;
+      break;
+
+    default:
+      _pad_type = TYPE_NONE;
+      break;
+  }
+}
+
+SaturnPad::Nibble SaturnPad::readNibble(uint8_t s0, uint8_t s1)
+{
+  digitalWrite(_s_pins[0], s0);
+  digitalWrite(_s_pins[1], s1);
+
+  Nibble nibble;
+  nibble.components.bit0 = digitalRead(_d_pins[0]);
+  nibble.components.bit1 = digitalRead(_d_pins[1]);
+  nibble.components.bit2 = digitalRead(_d_pins[2]);
+  nibble.components.bit3 = digitalRead(_d_pins[3]);
+  return nibble;
+}
+
+void SaturnPad::readDigital()
+{
+  Nibble nibble;
+
+  nibble = readNibble(HIGH, HIGH);
+  _digital_pad_state.id = nibble.nibble & 0x3;
+  _digital_pad_state.button_l = !nibble.components.bit3;
+
+  nibble = readNibble(HIGH, LOW);
+  _digital_pad_state.button_b = !nibble.components.bit0;
+  _digital_pad_state.button_c = !nibble.components.bit1;
+  _digital_pad_state.button_a = !nibble.components.bit2;
+  _digital_pad_state.button_start = !nibble.components.bit3;
+
+  nibble = readNibble(LOW, HIGH);
+  _digital_pad_state.button_up = !nibble.components.bit0;
+  _digital_pad_state.button_down = !nibble.components.bit1;
+  _digital_pad_state.button_left = !nibble.components.bit2;
+  _digital_pad_state.button_right = !nibble.components.bit3;
+
+  nibble = readNibble(LOW, LOW);
+  _digital_pad_state.button_z = !nibble.components.bit0;
+  _digital_pad_state.button_y = !nibble.components.bit1;
+  _digital_pad_state.button_x = !nibble.components.bit2;
+  _digital_pad_state.button_r = !nibble.components.bit3;
+}
+
+void SaturnPad::readAnalog()
+{
+}
+
---
+++ SaturnPad.h
@@ -0,0 +1,95 @@
+#ifndef SATURN_PAD_H
+#define SATURN_PAD_H
+
+#include <Arduino.h>
+
+class SaturnPad
+{
+  public:
+    struct DigitalPadState {
+      uint8_t id:3;
+
+      uint8_t button_start:1;
+      uint8_t button_up:1;
+      uint8_t button_down:1;
+      uint8_t button_left:1;
+      uint8_t button_right:1;
+      uint8_t button_a:1;
+      uint8_t button_b:1;
+      uint8_t button_c:1;
+      uint8_t button_x:1;
+      uint8_t button_y:1;
+      uint8_t button_z:1;
+      uint8_t button_l:1;
+      uint8_t button_r:1;
+    };
+
+    struct AnalogPadState {
+      uint8_t CTRL_BYTE_COUNT:4;
+      uint8_t CTRL_ID_3D:4;
+
+      uint8_t BTN_B:1;
+      uint8_t BTN_C:1;
+      uint8_t BTN_A:1;
+      uint8_t BTN_START:1;
+      uint8_t BTN_UP:1;
+      uint8_t BTN_DOWN:1;
+      uint8_t BTN_LEFT:1;
+      uint8_t BTN_RIGHT:1;
+
+      uint8_t CTRL_ID:3;
+      uint8_t BTN_L:1;
+      uint8_t BTN_Z:1;
+      uint8_t BTN_Y:1;
+      uint8_t BTN_X:1;
+      uint8_t BTN_R:1;
+
+      uint8_t ANLG_STICK_LR;
+      uint8_t ANLG_STICK_UD;
+      uint8_t ANLG_TRIGGER_R;
+      uint8_t ANLG_TRIGGER_L;
+    };
+
+    struct Nibble {
+      union {
+        uint8_t nibble:4;
+        struct {
+          uint8_t bit0:1;
+          uint8_t bit1:1;
+          uint8_t bit2:1;
+          uint8_t bit3:1;
+        } components;
+      };
+    };
+
+    enum PadType {
+      TYPE_NONE,
+      TYPE_DIGITAL,
+      TYPE_ANALOG
+    };
+
+    SaturnPad(uint8_t s0_pin, uint8_t s1_pin, uint8_t d0_pin, uint8_t d1_pin, uint8_t d2_pin, uint8_t d3_pin);
+
+    void begin();
+    void loop();
+
+    PadType padType();
+    DigitalPadState digitalPadState();
+    AnalogPadState analogPadState();
+
+  private:
+    void setType();
+    Nibble readNibble(uint8_t s0, uint8_t s1);
+    void readDigital();
+    void readAnalog();
+
+    uint8_t _s_pins[2];
+    uint8_t _d_pins[4];
+
+    PadType _pad_type;
+    DigitalPadState _digital_pad_state;
+    AnalogPadState _analog_pad_state;
+};
+
+#endif
+
